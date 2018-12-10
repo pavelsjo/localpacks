79cf46819368161a8f034ed0226e6743714a6a3e
pavelsjo <pavels.jo@gmail.com>
2018-12-10 09:37:20 -0300

    V1 localpacks templates

---
+++ data.py
@@ -0,0 +1,106 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+#FUENTE DE DATOS
+URL = {}
+
+FILENAME = {}
+
+#FUNCIONES
+def get_data(year = , filename = FILENAME, url = URL, force_download=False):
+    """Load and return the iris dataset (classification).
+
+    The iris dataset is a classic and very easy multi-class classification
+    dataset.
+
+    =================   ==============
+    Classes                          3
+    Samples per class               50
+    Samples total                  150
+    Dimensionality                   4
+    Features            real, positive
+    =================   ==============
+
+    Read more in the :ref:`User Guide <datasets>`.
+
+    Parameters
+    ----------
+    return_X_y : boolean, default=False.
+        If True, returns ``(data, target)`` instead of a Bunch object. See
+        below for more information about the `data` and `target` object.
+
+        .. versionadded:: 0.18
+
+    Returns
+    -------
+    data : Bunch
+        Dictionary-like object, the interesting attributes are:
+        'data', the data to learn, 'target', the classification labels,
+        'target_names', the meaning of the labels, 'feature_names', the
+        meaning of the features, and 'DESCR', the
+        full description of the dataset.
+
+    (data, target) : tuple if ``return_X_y`` is True
+
+        .. versionadded:: 0.18
+
+    Examples
+    --------
+    Let's say you are interested in the samples 10, 25, and 50, and want to
+    know their class name.
+
+    >>> from sklearn.datasets import load_iris
+    >>> data = load_iris()
+    >>> data.target[[10, 25, 50]]
+    array([0, 0, 1])
+    >>> list(data.target_names)
+    ['setosa', 'versicolor', 'virginica']
+    """
+    module_path = dirname(__file__)
+    data, target, target_names = load_data(module_path, 'iris.csv')
+
+    with open(join(module_path, 'descr', 'iris.rst')) as rst_file:
+        fdescr = rst_file.read()
+
+    if return_X_y:
+        return data, target
+
+    return Bunch(data=data, target=target,
+                 target_names=target_names,
+                 DESCR=fdescr,
+                 feature_names=['sepal length (cm)', 'sepal width (cm)',
+                                'petal length (cm)', 'petal width (cm)'])
+
+
+def format_data(df):
+    """
+    PARAMETERS
+    ----------
+    RETURN
+    ------
+    """
+    pass
+    return df = None
+
+def clean_garbage(df):
+    '''
+    PARAMETERS
+    ----------
+    df: pandas DataFrame (obligatorio)
+        toma un pandas DataFrame con datos crudos
+    RETURN
+    ------
+    clean: pandas.DataFrame
+       clean pandas DataFrame
+    null: pandas.DataFrame
+       pandas DataFrame with contains null values
+    duplicates: pandas.DataFrame
+       pandas DataFrame with contains duplicates values
+    outliers: pandas.DataFrame
+       pandas DataFrame with contains outlier values
+    '''
+    return clean=None, nulls=None, duplicates=None, outliers=None
+
+def panel_visualization():
+    pass
